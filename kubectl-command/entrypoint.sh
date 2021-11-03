4fca75db6dce832a8886dec508ef513124493a06
everops-ethan <ethan.post@everops.com>
2021-11-03 13:22:45 -0500

    added role-session-name

--- kubectl-command/entrypoint.sh
+++ kubectl-command/entrypoint.sh
@@ -7,7 +7,7 @@ echo $KUBE_CONFIG | base64 -d > ~/.kube/config
 
 aws sts get-caller-identity
 
-aws sts assume-role --role-arn arn:aws:iam::128863593841:role/deploy-platform-spinnaker-test-user-role
+aws sts assume-role --role-arn arn:aws:iam::128863593841:role/deploy-platform-spinnaker-test-user-role --role-session-name ga-runner-eks
 
 aws sts get-caller-identity
 
