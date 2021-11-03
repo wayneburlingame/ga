bf105f5f7aaced3ff929b49c2b949f169140b9cd
everops-ethan <ethan.post@everops.com>
2021-11-03 13:19:17 -0500

    testing aws assume role

--- kubectl-command/entrypoint.sh
+++ kubectl-command/entrypoint.sh
@@ -5,4 +5,10 @@ set -e
 mkdir -p ~/.kube
 echo $KUBE_CONFIG | base64 -d > ~/.kube/config
 
-"$@"
+aws sts get-caller-identity
+
+aws sts assume-role --role-arn arn:aws:iam::128863593841:role/deploy-platform-spinnaker-test-user-role
+
+aws sts get-caller-identity
+
+kubectl "$@"
