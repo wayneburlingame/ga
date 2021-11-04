d0558d6de170fcb6111709464f6dbf0a1e2a9d47
everops-ethan <ethan.post@everops.com>
2021-11-04 10:43:17 -0500

    removing aws-cli additions from action

--- kubectl-command/entrypoint.sh
+++ kubectl-command/entrypoint.sh
@@ -5,15 +5,4 @@ set -e
 mkdir -p ~/.kube
 echo $KUBE_CONFIG | base64 -d > ~/.kube/config
 
-aws sts get-caller-identity
-
-export $(printf "AWS_ACCESS_KEY_ID=%s AWS_SECRET_ACCESS_KEY=%s AWS_SESSION_TOKEN=%s" \
-                $(aws sts assume-role \
-                      --role-arn arn:aws:iam::128863593841:role/deploy-platform-spinnaker-test-user-role \
-                      --role-session-name ga-runner-eks \
-                      --query "Credentials.[AccessKeyId,SecretAccessKey,SessionToken]" \
-                      --output text))
-
-aws sts get-caller-identity
-
 kubectl "$@"
